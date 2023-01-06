16c21bd4de9e74ea430bb42fa5516990bde673ff
Antonio Gil <agil@fmit-solutions.com>
2023-01-06 18:23:33 +0100

    feat: create base project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Vue Best Practices</h1>
+  </header>
+
+  <RouterView />
+</template>
