026030d18cc4c392ebcaf9a2268cfdfedb431ec0
Antonio Gil <agil@fmit-solutions.com>
2023-01-17 06:59:29 +0100

    feat: add routes with base layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Layout from '../sections/layout/Layout.vue'
+import Dashboard from '../sections/dashboard/Dashboard.vue'
+import { config } from '../config'
+import { GitHubApiGitHubRepositoryRepository } from '../infrastructure/GitHubApiGitHubRepositoryRepository'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: Layout,
+      children: [
+        {
+          path: '',
+          component: Dashboard,
+          props: () => {
+            const repository = new GitHubApiGitHubRepositoryRepository(config.github_access_token)
+            return {
+              repository,
+            }
+          },
+        },
+      ],
+    },
+  ],
 })
 
 export default router
---
+++ src/sections/layout/Layout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Logo from '../../assets/logo.svg?component'
+import styles from './Layout.module.css'
+</script>
+
+<template>
+  <header :class="styles.header">
+    <section :class="styles.header__container">
+      <Logo />
+      <h1 :class="styles.app__brand">
+        DevDash_
+      </h1>
+    </section>
+  </header>
+
+  <RouterView />
+</template>
