d865eb70ec2f84c02f03e19e530b85981533b6b7
Antonio Gil <antonioluisgil@gmail.com>
2023-03-22 22:30:06 +0100

    feat: add loader component

---
+++ src/shared/components/Loader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import styles from './Loader.module.css'
+</script>
+
+<template>
+  <div :class="styles.loader__container">
+    <div :class="styles['lds-ellipsis']">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+</template>
