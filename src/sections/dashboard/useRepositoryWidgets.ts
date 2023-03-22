3bf54ba4ddb2601684206348b04d656346acf174
Antonio Gil <antonioluisgil@gmail.com>
2023-03-22 22:30:07 +0100

    feat: read widgets from localStorage

--- src/sections/dashboard/useRepositoryWidgets.ts
+++ src/sections/dashboard/useRepositoryWidgets.ts
@@ -1,11 +1,19 @@
-import { type Ref, ref } from 'vue'
+import { type Ref, onBeforeMount, ref } from 'vue'
 import type { RepositoryWidget } from '@/domain/RepositoryWidget'
 import { config } from '@/config'
+import type { RepositoryWidgetRepository } from '@/domain/RepositoryWidgetRepository'
 
 const repositoryWidgets = ref<RepositoryWidget[]>([])
 
-export function useRepositoryWidgets(): { repositoryWidgets: Ref<RepositoryWidget[]> } {
-  repositoryWidgets.value = config.widgets.map(widget => ({ id: widget.id, repositoryUrl: widget.repository_url }))
+export function useRepositoryWidgets(repositoryWidgetRepository: RepositoryWidgetRepository): { repositoryWidgets: Ref<RepositoryWidget[]> } {
+  onBeforeMount(async () => {
+    let widgets = await repositoryWidgetRepository.search() || []
+
+    if (widgets.length === 0)
+      widgets = config.widgets.map(widget => ({ id: widget.id, repositoryUrl: widget.repository_url }))
+
+    repositoryWidgets.value = widgets
+  })
 
   return {
     repositoryWidgets,
