3ae7b1f2dc5f1a4c5bf952ba1d03ace77292935e
Antonio Gil <antonioluisgil@gmail.com>
2023-03-22 22:30:06 +0100

    test: add tests for AddRepositoryWidget component

--- tests/index.ts
+++ tests/index.ts
@@ -1,11 +1,21 @@
-import type { RenderOptions } from '@testing-library/vue'
+import type { RenderOptions, RenderResult } from '@testing-library/vue'
 import { render as tlRender } from '@testing-library/vue'
 import { RouterLinkStub } from '@vue/test-utils'
+import UserEvent from '@testing-library/user-event'
 import router from '@/router'
+
+interface RenderOptionsWithUser { user: typeof UserEvent }
 
 export * from '@testing-library/vue'
 
-export const render = (testComponent: unknown, options: RenderOptions = {}) => {
+export function render(testComponent: unknown, options: RenderOptions = {}): RenderOptionsWithUser {
+  return {
+    user: UserEvent.setup() as unknown as typeof UserEvent,
+    ...tlRender(testComponent, options),
+  }
+}
+
+export function renderWithRouter(testComponent: unknown, options: RenderOptions = {}): RenderResult {
   return tlRender(testComponent, {
     ...options,
     global: {
